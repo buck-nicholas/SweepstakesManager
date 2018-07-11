e79fa65eea75c8db0206ba55a24cf92d0d00ed07
buck-nicholas <buck.nicholas.c@gmail.com>
2018-07-11 10:32:57 -0500

    Added: Contestant Class and respective member variables

---
+++ SweepstakesManager/Contestant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakesManager
+{
+    class Contestant
+    {
+        private string firstName;
+        private string lastName;
+        private string emailAddress;
+        private int registrationNumber;
+
+        public Contestant(string firstName, string lastName, string emailAddress, int registrationNumber)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.emailAddress = emailAddress;
+            this.registrationNumber = registrationNumber;
+        }
+    }
+}
