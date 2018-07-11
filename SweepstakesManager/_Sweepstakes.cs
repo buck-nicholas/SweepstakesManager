ac7a10b9484c0d1419361d3512da43105213bc66
buck-nicholas <buck.nicholas.c@gmail.com>
2018-07-11 15:51:19 -0500

    Added: SweepstakesQueueManager class and SweepstakesStackManager class

--- SweepstakesManager/_Sweepstakes.cs
+++ SweepstakesManager/_Sweepstakes.cs
@@ -8,18 +8,17 @@ namespace Sweepstakes
 {
     class _Sweepstakes
     {
-        Dictionary<int, string> contestants = new Dictionary<int, string>();
+        Dictionary<int, Contestant> contestants = new Dictionary<int, Contestant>();
         public string sweepstakesName;
 
         public _Sweepstakes()
         {
-
-
+            
         }
 
         public void RegisterContestant(Contestant contestant)
         {
-
+            contestants.Add(contestant.registrationNumber, contestant);
         }
 
         public string pickWinner()
