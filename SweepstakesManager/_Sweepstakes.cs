a5bcebfd005e88bf5a381bde4d108fcb0f4dcf8c
buck-nicholas <buck.nicholas.c@gmail.com>
2018-07-11 15:29:10 -0500

    Added: _Sweepstakes class and skeleton functions. Rebuilt again to attempt to fix namespace issue

---
+++ SweepstakesManager/_Sweepstakes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class _Sweepstakes
+    {
+        Dictionary<int, string> contestants = new Dictionary<int, string>();
+        public string sweepstakesName;
+
+        public _Sweepstakes()
+        {
+
+
+        }
+
+        public void RegisterContestant(Contestant contestant)
+        {
+
+        }
+
+        public string pickWinner()
+        {
+
+        }
+
+        public void PrintContestantInfo(Contestant contestant)
+        {
+
+        }
+        
+    }
+}
