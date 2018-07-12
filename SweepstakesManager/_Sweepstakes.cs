eba3ab0715a77105029b27df26e0914093197d06
buck-nicholas <buck.nicholas.c@gmail.com>
2018-07-12 09:49:12 -0500

    Completed functionality for _Sweepstakes class

--- SweepstakesManager/_Sweepstakes.cs
+++ SweepstakesManager/_Sweepstakes.cs
@@ -8,40 +8,38 @@ namespace Sweepstakes
 {
     class _Sweepstakes
     {
-        Dictionary<int, Contestant> contestants = new Dictionary<int, Contestant>();
+        Dictionary<Guid, Contestant> contestants = new Dictionary<Guid, Contestant>();
         public string sweepstakesName;
-        UserInterface userInterface = new UserInterface();
+        private readonly static Random randomNumber = new Random();
         Contestant contestant;
-
         public _Sweepstakes()
         {
-            
+            sweepstakesName = UserInterface.GetUserInput("Enter Sweepstakes Name: ");
         }
-        public void CreateContestant()
+        public void CreateNewContestant()
         {
-            contestant = new Contestant();
-            userInterface.GetContestantInformation();
+            Contestant newContestant = new Contestant();
+            RegisterContestant(newContestant);
         }
-
         public void RegisterContestant(Contestant contestant)
         {
             contestants.Add(contestant.registrationNumber, contestant);
         }
-
-        public string pickWinner()
+        public string PickWinner()
         {
-
+            contestants.Count();
+            Contestant winner = contestants.ElementAt(randomNumber.Next(contestants.Count - 1)).Value;
+            string sweepstakesWinner = winner.firstName + " " + winner.lastName;
+            return sweepstakesWinner;
         }
-
         public void PrintContestantInfo(Contestant contestant)
         {
-
+            Console.WriteLine("Information for contestant registration number: {0}\nName: {1} {2}\nEmail Address: {3}", contestant.registrationNumber, contestant.firstName, contestant.lastName, contestant.emailAddress);
         }
         public void SetSweepStakesName()
         {
             Console.Write("Sweepstakes Name: ");
             Console.ReadLine();
         }
-        
     }
 }
